f1217b08cf256bab8f1df63fe12bffaa5e27f73f
codedogQBY <1369175442@qq.com>
2025-07-22 00:54:50 +0800

    feat:删除配置

--- next.config.ts
+++ next.config.ts
@@ -100,23 +100,6 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-        ],
-      },
-      {
         source: '/_next/image(.*)',
         headers: [
           {
